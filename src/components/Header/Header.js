aa1db0c03eea34342ae15e741546f17006037b17
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-05-20 21:46:40 +0600

    data made

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <h2>Diamond shop</h2>
+            </div>
+            <div>
+                {/* <a href="">f</a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a> */}
+            </div>
+        </div>
+    );
+};
+
+export default Header;
