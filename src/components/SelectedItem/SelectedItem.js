ce94a89cb46ddc3ce86f6c08fc9b4ae21a7d2368
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-05-21 18:12:08 +0600

    item selection updated

--- src/components/SelectedItem/SelectedItem.js
+++ src/components/SelectedItem/SelectedItem.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './SelectedItem.css'
 const SelectedItem = ({ selected }) => {
     console.log(selected);
+    let name = ''
+    for (const product of selected) {
+        name = name + product.name + ',';
+    }
     // const { selected } = props.selected
     return (
         <div>
-            <h1>{selected.length}</h1>
+            <h1>Selected Rings : {selected.length}</h1>
+            <h1>{name}</h1>
 
         </div>
     );
