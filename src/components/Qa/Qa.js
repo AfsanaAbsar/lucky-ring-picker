0ab2fb72a61a8e2dae84b494e39a8b5d7dcb1b0a
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-05-21 20:07:26 +0600

    qa updated

---
+++ src/components/Qa/Qa.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Qa.css'
+const Qa = () => {
+    return (
+        <div className='qa'>
+            <h1>How React Works?</h1>
+            <p>== As React is a frontend Js library, it makes the development of user interface easier. it is a component based library.React creates a list of those changes that need to be made and make sure to do all the changes in a single process.It helps to update things in a website without loading it </p>
+
+            <h1>How useState Works?</h1>
+            <p>== useState is a function in react that helps to store data and returns an array.The array has two entries,first one helps to declare the initial state and second one helps to update the state</p>
+        </div>
+    );
+};
+
+export default Qa;
